0737b9aff85ed06d4104aec4fe968e3904d960ed
Mark Pustjens <mark@unkie.org>
2007-08-26 15:44:28 +0200

    implemented framework for input processing

---
+++ cppconsui/InputProcessor.cpp
@@ -0,0 +1,136 @@
+/* This file is part of CenterIM.
+ *
+ * CenterIM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CenterIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
+#include "InputProcessor.h"
+
+InputProcessor::InputProcessor(InputProcessor *processchild)
+: processchild(processchild)
+{
+}
+
+InputProcessor::~InputProcessor()
+{
+}
+
+int InputProcessor::ProcessInput(const char *input, const int bytes)
+{
+	int i;
+
+	/* Process overriding key combinations first */
+	i = Process(Override, input, bytes);
+	if (i) return i;
+	
+	/* Hand of input to a child */
+	if (processchild)
+		i = processchild->ProcessInput(input, bytes);
+	if (i) return i;
+
+	/* Process other key combinations */
+	i = Process(Normal, input, bytes);
+	if (i) return i;
+
+	/* Do non-combo input processing */
+	return ProcessInputText(input, bytes);
+}
+
+int InputProcessor::ProcessInputText(const char *input, const int bytes)
+{
+	return 0;
+}
+
+int InputProcessor::Process(InputProcessor::ComboType type, const char *input, const int bytes)
+{
+	KeyCombos::iterator i, begin, end;
+	KeyCombo combo;
+	int m;
+
+	begin = BinSearchFirst(input);
+	end = BinSearchLast(input);
+
+	for (i = begin; i != end; i++) {
+		combo = *i;
+		if (combo.type == type || type == Both) {
+			m = Match(combo.key, input, bytes);
+			if (m < 0) {
+				/* could match, but need btes more input to be sure */
+				return m;
+			} else if (m > 0) {
+				/* found a match */
+				combo.sig->emit();
+				return m;
+			} else {
+				/* do nothing */
+			}
+		}
+	}
+
+	return 0;
+}
+
+int InputProcessor::Match(const std::string skey, const char *input, const int bytes)
+{
+	const char *key = skey.c_str();
+	int n;
+	
+	n = skey.size();
+	if (bytes < n) n = bytes;
+
+	if (strncmp(key, input, n) == 0) {
+		if (bytes < skey.size())
+			return bytes - skey.size(); /* need more input to determine a match */
+		else
+			return n; /* complete match found */
+	} else {
+		return 0;
+	}
+}
+
+InputProcessor::KeyCombos::iterator InputProcessor::BinSearchFirst(const char *key)
+{
+	int m, n, h;
+
+	m, n = -1, combos.size();
+
+	while (m+1 < n) {
+		h = (m+n) / 2;
+
+		if (combos[h].key[0] >= key[0])
+			n = h;
+		else
+			m = h;
+	}
+
+	return combos.begin() + n;
+}
+
+InputProcessor::KeyCombos::iterator InputProcessor::BinSearchLast(const char *key)
+{
+	int m, n, h;
+
+	m, n = -1, combos.size();
+
+	while (m+1 < n) {
+		h = (m+n) / 2;
+
+		if (combos[h].key[0] > key[0])
+			n = h;
+		else
+			m = h;
+	}
+
+	return combos.begin() + n;
+}
---
+++ cppconsui/InputProcessor.h
@@ -0,0 +1,91 @@
+/* This file is part of CenterIM.
+ *
+ * CenterIM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CenterIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
+#ifndef __INPUTPROCESSOR_H__
+#define __INPUTPROCESSOR_H__
+
+#include <sigc++/sigc++.h>
+#include <sigc++/signal.h>
+
+#include <vector>
+#include <string>
+
+class InputProcessor
+{
+	public:
+		InputProcessor(InputProcessor *processchild);
+		~InputProcessor();
+
+		/* Notes on how input is processed
+		 *
+		 * Input is processed by first checking for overriding
+		 * key combinations. If a match is found, the signal for
+		 * that combo is executed, and the function returns 
+		 * the number of bytes of that combo.
+		 *
+		 * If no match is found the ProcessInput function of
+		 * a child, if not NULL is called. If this returns a
+		 * non-zero value the functions returns that value.
+		 *
+		 * If no input was eaten by the child, or no child was
+		 * selected normal key combinations are checked.
+		 * If a match is found, the signal for that combo is
+		 * executed, and the function returns the number of
+		 * bytes of that combo.
+		 *
+		 * TODO update and make accurate this description
+		 * */
+		int ProcessInput(const char *input, const int bytes);
+
+	protected:
+		virtual int ProcessInputText(const char *input, const int bytes);
+
+	private:
+		/* Notes on how key combinations are stored and searched
+		 *
+		 * Key combinations are stored in a vector (array) stabally
+		 * sorted on the first byte of the key value only.
+		 *
+		 * This allows for fast searching using binary search. Inserts
+		 * are slower, but since key combinations are manipulated
+		 * very irregular this is not a problem.
+		 *
+		 * When adding a new combination to the vector, care must be
+		 * taken to look for `shadowing'. This occurs when when to
+		 * key combinations have the same prefix. In this case the
+		 * first definition is used. The user should be notified of
+		 * an error in the configuration.
+		 * */
+		enum ComboType {Normal, Override, Both};
+		typedef struct KeyCombo {
+			std::string key;
+			sigc::signal<void> *sig;
+			ComboType type;
+		};
+		typedef std::vector<KeyCombo> KeyCombos;
+
+		int Process(ComboType type, const char *input, const int bytes);
+		int Match(const std::string skey, const char *input, const int bytes);
+
+		KeyCombos::iterator BinSearchFirst(const char *key);
+		KeyCombos::iterator BinSearchLast(const char *key);
+
+		KeyCombos combos;
+		InputProcessor *processchild;
+};
+
+#endif /* __INPUTPROCESSOR_H__ */
