e19d1dfe53ed87c3b66bf7b9f923d1f04fc5ccb2
Mark Pustjens <mark@unkie.org>
2008-04-17 21:00:57 +0200

    Added basics of color schemes - Missing files.

---
+++ cppconsui/ColorScheme.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2008 by Mark Pustjens <pustjens@dds.nl>
+ *
+ * This file is part of CenterIM.
+ *
+ * CenterIM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CenterIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
+#include "ColorScheme.h"
+
+ColorScheme::ColorScheme()
+{
+	for (int i = 0; i <= Disabled; i++) {
+		schemes[i] = 0;
+	}
+}
+
+ColorScheme::ColorScheme(const ColorScheme *colorscheme)
+{
+	for (int i = 0; i <= Disabled; i++) {
+		schemes[i] = colorscheme->schemes[i];
+	}
+}
+
+ColorScheme::~ColorScheme(void)
+{
+}
+
+ColorScheme* ColorScheme::ColorSchemeNormal(void)
+{
+	ColorScheme *scheme = new ColorScheme();
+	scheme->schemes[Normal]   = A_NORMAL;
+	scheme->schemes[Focus]    = A_REVERSE;
+	scheme->schemes[Disabled] = A_DIM;
+	return scheme;
+}
+
+void ColorScheme::On(const curses_imp_t* area, const ColorType type)
+{
+	wattron(area->w, schemes[type]);
+}
+
+void ColorScheme::Off(const curses_imp_t* area, const ColorType type)
+{
+	wattroff(area->w, schemes[type]);
+}
---
+++ cppconsui/ColorScheme.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2008 by Mark Pustjens <pustjens@dds.nl>
+ *
+ * This file is part of CenterIM.
+ *
+ * CenterIM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CenterIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
+#ifndef __COLORSCHEME_H__
+#define __COLORSCHEME_H__
+
+#if defined(USE_NCURSES) && !defined(RENAMED_NCURSES)
+//#define _XOPEN_SOURCE_EXTENDED
+#include <ncurses.h>
+#else
+#include <curses.h>
+#endif
+
+#include "Curses.h"
+
+class ColorScheme
+{
+	public:
+		enum ColorType {Normal = 0, Focus, Disabled};
+
+		ColorScheme(void);
+		ColorScheme(const ColorScheme *colorscheme);
+		~ColorScheme();
+
+		#define ColorSchemeDefault ColorSchemeNormal
+		static ColorScheme* ColorSchemeNormal(void);
+
+		void On(const curses_imp_t* area, const ColorType type);
+		void Off(const curses_imp_t* area, const ColorType type);
+
+		void SetColorType(ColorType type, int scheme);
+		void GetColorType(ColorType type);
+
+	protected:
+		int schemes[Disabled];
+
+	private:
+		ColorScheme(const ColorScheme&);
+
+		ColorScheme& operator=(ColorScheme&);
+};
+
+#endif /* __COLORSCHEME_H__ */
