40f7dfb24404eda6dfc87b512d59d9e5e4bd1278
Alexander Adam <alexander.adam@informatik.tu-chemnitz.de>
2010-09-28 18:14:44 +0200

    Added CMake based build system side by side with existing automake based one. Easing use of tools like QtCreator and Eclipse CDE. What does not yet work:  - installing            - configuring of versioning information

---
+++ cmake/CMakeConfs.cmake
@@ -0,0 +1,86 @@
+#####################################################################
+##              handling of package config libraries               ##
+#####################################################################
+
+find_package( PkgConfig)
+pkg_check_modules(GLIB2 glib-2.0)
+pkg_check_modules(PURPLE purple)
+
+#####################################################################
+##                    handling sigc++ library                      ##
+#####################################################################
+
+include( cmake/FindSigc.cmake)
+
+#####################################################################
+##              handling of (n)curses wide character               ##
+#####################################################################
+
+set( CURSES_NEED_WIDE TRUE)
+include( cmake/FindCursesw.cmake)
+
+# as the curses script delivers not the real wide character include
+# directory, we now have to extract the right information
+
+get_filename_component(	CURSESW_DIR
+			${CURSES_HAVE_NCURSESW_CURSES_H}
+			PATH
+		)
+get_filename_component(	NCURSESW_DIR
+			${CURSES_HAVE_NCURSESW_NCURSES_H}
+			PATH
+		)
+
+if ( IS_DIRECTORY ${CURSESW_DIR})
+
+	set( CURSES_INCLUDE_DIR ${CURSESW_DIR})
+
+else ( IS_DIRECTORY ${CURSESW_DIR})
+
+	if ( IS_DIRECTORY ${NCURSESW_DIR})
+
+		set( CURSES_INCLUDE_DIR ${NCURSESW_DIR})
+
+	else ( IS_DIRECTORY ${NCURSESW_DIR})
+
+		message( SEND_ERROR "No widecharacter version of curses")
+
+	endif ( IS_DIRECTORY ${NCURSESW_DIR})
+
+endif ( IS_DIRECTORY ${CURSESW_DIR})
+
+#####################################################################
+##                   generation of config file                     ##
+#####################################################################
+
+configure_file(	config.in.h config.h)
+
+#####################################################################
+##                  populating include directories                 ##
+#####################################################################
+
+include_directories(	${CURSES_INCLUDE_DIR}
+			${GLIB2_INCLUDE_DIRS}
+			${PURPLE_INCLUDE_DIRS}
+			${LIBSIGC_INCLUDE_DIRS}
+			${centerim_BINARY_DIR}
+			${centerim_SOURCE_DIR}
+		)
+
+#####################################################################
+##                    coping with translations                     ##
+#####################################################################
+
+find_package( Gettext)
+
+if (GETTEXT_FOUND)
+	message( "-- Found Gettext: " ${GETTEXT_MSGMERGE_EXECUTABLE} " " ${GETTEXT_MSGFMT_EXECUTABLE} )
+else (GETTEXT_FOUND)
+	message( SEND_ERROR "-- Gettext not found, translation not possible")
+endif (GETTEXT_FOUND)
+
+#####################################################################
+##                   coping with documentation                     ##
+#####################################################################
+
+find_package( Doxygen)
---
+++ config.in.h
@@ -0,0 +1,25 @@
+/* Name of package */
+#define PACKAGE "centerim"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://sourceforge.net/projects/centerim"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "centerim"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "centerim 5.0-alpha2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "centerim"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "5.0-alpha2"
+
+/* Version number of package */
+#define VERSION "5.0-alpha2"
+
+
