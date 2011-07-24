816528a93a197d4fe6dbfd1af77ff7b7f879b0dc
Petr Pavlu <setup@dagobah.cz>
2011-07-24 12:50:08 +0200

    Initial authorization code

--- src/Accounts.cpp
+++ src/Accounts.cpp
@@ -33,6 +33,39 @@ Accounts *Accounts::Instance()
   return instance;
 }
 
+Accounts::AddUserData::AddUserData(PurpleAccount *account_,
+    const char *username_, const char *alias_)
+: account(account_)
+{
+  username = g_strdup(username_);
+  alias = g_strdup(alias_);
+}
+
+Accounts::AddUserData::~AddUserData()
+{
+  if (username)
+    g_free(username);
+  if (alias)
+    g_free(alias);
+}
+
+Accounts::AuthAndAdd::AuthAndAdd(PurpleAccountRequestAuthorizationCb auth_cb_,
+    PurpleAccountRequestAuthorizationCb deny_cb_, void *data_,
+    const char *username_, const char *alias_, PurpleAccount *account_)
+: auth_cb(auth_cb_), deny_cb(deny_cb_), data(data_), account(account_)
+{
+  username = g_strdup(username_);
+  alias = g_strdup(alias_);
+}
+
+Accounts::AuthAndAdd::~AuthAndAdd()
+{
+  if (username)
+    g_free(username);
+  if (alias)
+    g_free(alias);
+}
+
 Accounts::Accounts()
 {
   // if the statuses are not known, set them all to the default
@@ -44,11 +77,11 @@ Accounts::Accounts()
 
   // set the purple account callbacks
   memset(&centerim_account_ui_ops, 0, sizeof(centerim_account_ui_ops));
-  //centerim_account_ui_ops.notify_added = notify_added_;
+  centerim_account_ui_ops.notify_added = notify_added_;
   centerim_account_ui_ops.status_changed = status_changed_;
-  //centerim_account_ui_ops.request_add = request_add_;
-  //centerim_account_ui_ops.request_authorize = request_authorize_;
-  //centerim_account_ui_ops.close_account_request = close_account_request_;
+  centerim_account_ui_ops.request_add = request_add_;
+  centerim_account_ui_ops.request_authorize = request_authorize_;
+  centerim_account_ui_ops.close_account_request = close_account_request_;
   purple_accounts_set_ui_ops(&centerim_account_ui_ops);
 }
 
@@ -72,6 +105,36 @@ void Accounts::Finalize()
   instance = NULL;
 }
 
+char *Accounts::MakeInfo(PurpleAccount *account, const char *remote_user,
+    const char *id, const char *alias, const char *msg)
+{
+  if (msg && !*msg)
+    msg = NULL;
+
+  char *ins = NULL;
+  if (alias)
+    ins = g_strdup_printf(" (%s)", alias);
+
+  const char *name = id;
+  if (!name)
+    name = purple_account_get_name_for_display(account);
+
+  char *res = g_strdup_printf(_("%s%s has made %s his or her buddy%s%s"),
+      remote_user, ins ? ins : "",  name , msg ? ":\n" : ".",
+      msg ? msg  : "");
+  if (ins)
+    g_free(ins);
+  return res;
+}
+
+void Accounts::notify_added(PurpleAccount *account, const char *remote_user,
+    const char *id, const char *alias, const char *message)
+{
+  char *buf = MakeInfo(account, remote_user, id, alias, message);
+  LOG->Message(buf);
+  g_free(buf);
+}
+
 void Accounts::status_changed(PurpleAccount *account, PurpleStatus *status)
 {
   if (!purple_account_get_enabled(account, PACKAGE_NAME))
@@ -81,4 +144,102 @@ void Accounts::status_changed(PurpleAccount *account, PurpleStatus *status)
       purple_account_get_protocol_name(account),
       purple_account_get_username(account),
       purple_status_get_name(status));
+}
+
+void Accounts::request_add(PurpleAccount *account, const char *remote_user,
+    const char *id, const char *alias, const char *message)
+{
+  AddUserData *data = new AddUserData(account, remote_user, alias);
+  char *buf = MakeInfo(account, remote_user, id, alias, message);
+  purple_request_action(NULL, NULL, _("Add buddy to your list?"), buf,
+      PURPLE_DEFAULT_ACTION_NONE, account, remote_user, NULL, data, 2,
+      _("Add"), G_CALLBACK(add_user_cb_), _("Cancel"),
+      G_CALLBACK(add_user_cancel_cb_));
+  g_free(buf);
+}
+
+void *Accounts::request_authorize(PurpleAccount *account,
+    const char *remote_user, const char *id, const char *alias,
+    const char *message, gboolean on_list,
+    PurpleAccountRequestAuthorizationCb authorize_cb,
+    PurpleAccountRequestAuthorizationCb deny_cb,
+    void *user_data)
+{
+  if (message && !*message)
+    message = NULL;
+
+  char *ins = NULL;
+  if (alias)
+    ins = g_strdup_printf(" (%s)", alias);
+
+  const char *name = id;
+  if (!name)
+    name = purple_account_get_name_for_display(account);
+
+  char *buf = g_strdup_printf(
+      _("%s%s wants to add you (%s) to his or her buddy list%s%s"),
+      remote_user, ins ? ins : "" , name, message ? ":\n" : ".",
+      message ? message  : "");
+
+  if (ins)
+    g_free(ins);
+
+  void *ui_handle;
+  /* TODO Provide more information about the user that requested the
+   * authorization. */
+  if (on_list)
+    ui_handle = purple_request_action(NULL, _("Authorize buddy?"), buf, NULL,
+        PURPLE_DEFAULT_ACTION_NONE, account, remote_user, NULL, user_data, 2,
+        _("Authorize"), authorize_cb, _("Deny"), deny_cb);
+  else {
+    AuthAndAdd *aa = new AuthAndAdd(authorize_cb, deny_cb, user_data,
+        remote_user, alias, account);
+
+    ui_handle = purple_request_action(NULL, _("Authorize buddy?"), buf, NULL,
+        PURPLE_DEFAULT_ACTION_NONE, account, remote_user, NULL, aa, 2,
+        _("Authorize"), authorize_and_add_cb_, _("Deny"), deny_no_add_cb_);
+  }
+  g_free(buf);
+
+  // this is ugly
+  CppConsUI::FreeWindow *win
+    = reinterpret_cast<CppConsUI::FreeWindow*>(ui_handle);
+  win->signal_close.connect(sigc::mem_fun(this,
+        &Accounts::OnRequestAuthorizeClose));
+
+  return ui_handle;
+}
+
+void Accounts::close_account_request(void *ui_handle)
+{
+}
+
+void Accounts::add_user_cb(AddUserData *data)
+{
+  purple_blist_request_add_buddy(data->account, data->username, NULL,
+      data->alias);
+  delete data;
+}
+
+void Accounts::add_user_cancel_cb(AddUserData *data)
+{
+  delete data;
+}
+
+void Accounts::OnRequestAuthorizeClose(CppConsUI::FreeWindow& win)
+{
+  purple_account_request_close(&win);
+}
+
+void Accounts::authorize_and_add_cb(AuthAndAdd *aa)
+{
+  aa->auth_cb(aa->data);
+  purple_blist_request_add_buddy(aa->account, aa->username, NULL, aa->alias);
+  delete aa;
+}
+
+void Accounts::deny_no_add_cb(AuthAndAdd *aa)
+{
+  aa->deny_cb(aa->data);
+  delete aa;
 }
--- src/Accounts.h
+++ src/Accounts.h
@@ -22,6 +22,7 @@
 #ifndef __ACCOUNTS_H__
 #define __ACCOUNTS_H__
 
+#include <cppconsui/FreeWindow.h>
 #include <libpurple/purple.h>
 
 #define ACCOUNTS (Accounts::Instance())
@@ -36,6 +37,32 @@ protected:
 private:
   PurpleAccountUiOps centerim_account_ui_ops;
 
+  struct AddUserData
+  {
+    PurpleAccount *account;
+    char *username;
+    char *alias;
+
+    AddUserData(PurpleAccount *account_, const char *username_,
+        const char *alias_);
+    ~AddUserData();
+  };
+
+  struct AuthAndAdd
+  {
+    PurpleAccountRequestAuthorizationCb auth_cb;
+    PurpleAccountRequestAuthorizationCb deny_cb;
+    void *data;
+    char *username;
+    char *alias;
+    PurpleAccount *account;
+
+    AuthAndAdd(PurpleAccountRequestAuthorizationCb auth_cb_,
+        PurpleAccountRequestAuthorizationCb deny_cb_, void *data_,
+        const char *username_, const char *alias_, PurpleAccount *account_);
+    ~AuthAndAdd();
+  };
+
   static Accounts *instance;
 
   Accounts();
@@ -47,10 +74,55 @@ private:
   static void Finalize();
   friend class CenterIM;
 
+  char *MakeInfo(PurpleAccount *account, const char *remote_user,
+      const char *id, const char *alias, const char *msg);
+
+  static void notify_added_(PurpleAccount *account, const char *remote_user,
+      const char *id, const char *alias, const char *message)
+    { ACCOUNTS->notify_added(account, remote_user, id, alias, message); }
   static void status_changed_(PurpleAccount *account, PurpleStatus *status)
     { ACCOUNTS->status_changed(account, status); }
+  static void request_add_(PurpleAccount *account, const char *remote_user,
+      const char *id, const char *alias, const char *message)
+    { ACCOUNTS->request_add(account, remote_user, id, alias, message); }
+	static void *request_authorize_(PurpleAccount *account,
+      const char *remote_user, const char *id, const char *alias,
+      const char *message, gboolean on_list,
+      PurpleAccountRequestAuthorizationCb authorize_cb,
+      PurpleAccountRequestAuthorizationCb deny_cb, void *user_data)
+    { return ACCOUNTS->request_authorize(account, remote_user, id, alias,
+        message, on_list, authorize_cb, deny_cb, user_data); }
+	static void close_account_request_(void *ui_handle)
+    { ACCOUNTS->close_account_request(ui_handle); }
 
+  void notify_added(PurpleAccount *account, const char *remote_user,
+      const char *id, const char *alias, const char *message);
   void status_changed(PurpleAccount *account, PurpleStatus *status);
+  void request_add(PurpleAccount *account, const char *remote_user,
+      const char *id, const char *alias, const char *message);
+  void *request_authorize(PurpleAccount *account, const char *remote_user,
+      const char *id, const char *alias, const char *message,
+      gboolean on_list, PurpleAccountRequestAuthorizationCb authorize_cb,
+      PurpleAccountRequestAuthorizationCb deny_cb, void *user_data);
+	void close_account_request(void *ui_handle);
+
+  static void add_user_cb_(AddUserData *data)
+    { ACCOUNTS->add_user_cb(data); }
+  static void add_user_cancel_cb_(AddUserData *data)
+    { ACCOUNTS->add_user_cancel_cb(data); }
+
+  void add_user_cb(AddUserData *data);
+  void add_user_cancel_cb(AddUserData *data);
+
+  void OnRequestAuthorizeClose(CppConsUI::FreeWindow& win);
+
+  static void authorize_and_add_cb_(AuthAndAdd *aa)
+    { ACCOUNTS->authorize_and_add_cb(aa); }
+  static void deny_no_add_cb_(AuthAndAdd *aa)
+    { ACCOUNTS->deny_no_add_cb_(aa); }
+
+  void authorize_and_add_cb(AuthAndAdd *aa);
+  void deny_no_add_cb(AuthAndAdd *aa);
 };
 
 #endif // __ACCOUNTS_H__
