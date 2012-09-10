5214dbc4370582d154edaf1e94fd68716c2f1d95
Petr Pavlu <setup@dagobah.cz>
2012-09-10 22:25:03 +0200

    Update cmake build
    * Put all cmake files into autotools distribution package.
    * Get rid of bootstrap_cmake.
    * Fix dependency versions.
    * Simplify script for finding the ncurses library.
    * Fix install targets.
    * Fix git-version-gen logic.
    * Code style cleanup.

--- cmake/CMakeConfs.cmake
+++ cmake/CMakeConfs.cmake
@@ -2,83 +2,52 @@
 ##              handling of package config libraries               ##
 #####################################################################
 
-find_package( PkgConfig)
-pkg_check_modules(GLIB2 glib-2.0)
-pkg_check_modules(PURPLE purple)
-pkg_check_modules(SIGC sigc++-2.0)
+find_package(PkgConfig)
+pkg_check_modules(PURPLE REQUIRED "purple >= 2.7.0")
+pkg_check_modules(GLIB2 REQUIRED "glib-2.0 >= 2.16.0")
+pkg_check_modules(SIGC REQUIRED "sigc++-2.0 >= 2.2.0")
 
 #####################################################################
 ##              handling of (n)curses wide character               ##
 #####################################################################
 
-set( CURSES_NEED_WIDE TRUE)
-find_package( Cursesw)
-
-# as the curses script delivers not the real wide character include
-# directory, we now have to extract the right information
-
-get_filename_component(	CURSESW_DIR
-			${CURSES_HAVE_NCURSESW_CURSES_H}
-			PATH
-		)
-get_filename_component(	NCURSESW_DIR
-			${CURSES_HAVE_NCURSESW_NCURSES_H}
-			PATH
-		)
-
-if ( IS_DIRECTORY ${CURSESW_DIR})
-
-	set( CURSES_INCLUDE_DIR ${CURSESW_DIR})
-
-else ( IS_DIRECTORY ${CURSESW_DIR})
-
-	if ( IS_DIRECTORY ${NCURSESW_DIR})
-
-		set( CURSES_INCLUDE_DIR ${NCURSESW_DIR})
-
-	else ( IS_DIRECTORY ${NCURSESW_DIR})
-
-		message( SEND_ERROR "No widecharacter version of curses")
-
-	endif ( IS_DIRECTORY ${NCURSESW_DIR})
-
-endif ( IS_DIRECTORY ${CURSESW_DIR})
+set(Curses_FIND_REQUIRED TRUE)
+find_package(Cursesw)
 
 #####################################################################
 ##                  populating include directories                 ##
 #####################################################################
 
-include_directories(	${CURSES_INCLUDE_DIR}
-			${GLIB2_INCLUDE_DIRS}
-			${PURPLE_INCLUDE_DIRS}
-			${SIGC_INCLUDE_DIRS}
-			${centerim_BINARY_DIR}
-			${centerim_SOURCE_DIR}
-		)
+include_directories(
+  ${CURSES_INCLUDE_DIRS}
+  ${GLIB2_INCLUDE_DIRS}
+  ${PURPLE_INCLUDE_DIRS}
+  ${SIGC_INCLUDE_DIRS}
+  ${centerim5_BINARY_DIR}
+  ${centerim5_SOURCE_DIR})
 
 #####################################################################
 ##                    coping with translations                     ##
 #####################################################################
 
-find_package( Gettext)
+find_package(Gettext)
 
 if (GETTEXT_FOUND)
-	message( STATUS "-- Found Gettext: " ${GETTEXT_MSGMERGE_EXECUTABLE} " " ${GETTEXT_MSGFMT_EXECUTABLE} )
-	set( centerim_LOCALE_DIR \"${CMAKE_INSTALL_PREFIX}/share/locale/\" )
-	set( ENABLE_NLS true )
+  set(centerim5_LOCALE_DIR ${CMAKE_INSTALL_PREFIX}/share/locale)
+  set(ENABLE_NLS TRUE)
 else (GETTEXT_FOUND)
-	message( SEND_ERROR "-- Gettext not found, translation not possible")
-	set( ENABLE_NLS false )
+  message(SEND_ERROR "Gettext not found, translation not possible")
+  set(ENABLE_NLS FALSE)
 endif (GETTEXT_FOUND)
 
 #####################################################################
 ##                   coping with documentation                     ##
 #####################################################################
 
-find_package( Doxygen)
+find_package(Doxygen)
 
 #####################################################################
 ##                   generation of config file                     ##
 #####################################################################
 
-configure_file(	config.in.h config.h)
+configure_file(config.h.cmake config.h)
---
+++ config.h.cmake
@@ -0,0 +1,8 @@
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "${CMAKE_PROJECT_NAME}"
+
+/* Directory containing translations. */
+#define LOCALEDIR "${centerim5_LOCALE_DIR}"
+
+/* See if to use NLS features. */
+#cmakedefine01 ENABLE_NLS
