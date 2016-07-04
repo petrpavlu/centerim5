60106378d21b020a13d60d3df96c9fc972a023f4
Petr Pavlu <setup@dagobah.cz>
2016-07-04 20:11:33 +0100

    Teach the cmake build about ncursesw6-config

--- cmake/FindNCURSESW.cmake
+++ cmake/FindNCURSESW.cmake
@@ -1,18 +1,19 @@
-# Find ncursesw using ncursesw5-config. Set <CMAKE_FIND_PACKAGE_NAME>_FOUND,
-# <CMAKE_FIND_PACKAGE_NAME>_CFLAGS and <CMAKE_FIND_PACKAGE_NAME>_LDFLAGS in
-# the same way pkg_check_modules() does.
+# Find ncursesw using ncursesw6-config or ncursesw5-config. Set
+# <CMAKE_FIND_PACKAGE_NAME>_FOUND, <CMAKE_FIND_PACKAGE_NAME>_CFLAGS and
+# <CMAKE_FIND_PACKAGE_NAME>_LDFLAGS in the same way pkg_check_modules() does.
 
 if(${CMAKE_FIND_PACKAGE_NAME}_CFLAGS OR ${CMAKE_FIND_PACKAGE_NAME}_LDFLAGS)
   # Use externally set variables.
   set(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY true)
 else()
-  # Try to use ncursesw5-config to obtain correct flags.
-  find_program(NCURSESW5_CONFIG_EXECUTABLE ncursesw5-config)
-  if(NCURSESW5_CONFIG_EXECUTABLE)
-    execute_process(COMMAND ${NCURSESW5_CONFIG_EXECUTABLE} --cflags
+  # Try to use ncursesw6-config or ncursesw5-config to obtain correct flags.
+  find_program(NCURSESW_CONFIG_EXECUTABLE
+    NAMES ncursesw6-config ncursesw5-config)
+  if(NCURSESW_CONFIG_EXECUTABLE)
+    execute_process(COMMAND ${NCURSESW_CONFIG_EXECUTABLE} --cflags
       OUTPUT_VARIABLE ${CMAKE_FIND_PACKAGE_NAME}_CFLAGS
       OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND ${NCURSESW5_CONFIG_EXECUTABLE} --libs
+    execute_process(COMMAND ${NCURSESW_CONFIG_EXECUTABLE} --libs
       OUTPUT_VARIABLE ${CMAKE_FIND_PACKAGE_NAME}_LDFLAGS
       OUTPUT_STRIP_TRAILING_WHITESPACE)
     set(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY true)
@@ -24,4 +25,4 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
   REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY)
 
-mark_as_advanced(NCURSESW5_CONFIG_EXECUTABLE)
+mark_as_advanced(NCURSESW_CONFIG_EXECUTABLE)
